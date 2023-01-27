f8141907ab06e895f9194b21b8ee9da3ecd854ad
Buddy25112 <101816877+Buddy25112@users.noreply.github.com>
2023-01-27 02:25:35 -0600

    Update MS2StatTracker.lua

--- MS2StatTracker.lua
+++ MS2StatTracker.lua
@@ -2,12 +2,11 @@ if game.PlaceId == 9551640993 then
 	local username = game:GetService("Players").LocalPlayer.Name
 	local userid = game:GetService("Players").LocalPlayer.UserId
 	local icon = "https://www.roblox.com/headshot-thumbnail/image?userId="..userid.."&width=420&height=420&format=png"
-	local NewColor = (_G.Color == "" and "fffff") or _G.Color
+	local NewColor = (_G.SettingsTable.Color == "None" and "fffff") or _G.SettingsTable.Color
 	local number = 1
 	local number2 = 1
 	local number3 = 1
 	
-	if _G.SendNotifications == true then
     	game.StarterGui:SetCore(
                	"SendNotification",
                 {
@@ -16,8 +15,6 @@ if game.PlaceId == 9551640993 then
 			Duration = 5
 		}
 	)
-	elseif _G.SendNotifications == false then
-        end
 	
 	function abb(Value)
 		local Number
@@ -63,7 +60,7 @@ if game.PlaceId == 9551640993 then
    
     print("MS2 Stats Tracker V1.0.0, Maintained and Updated by PetSimulatorXPlayer#5011")
     
-    while wait(_G.Intervals) do
+    while wait(_G.SettingsTable.Intervals) do
         
         -- Eggs
         count1 = 0
@@ -102,7 +99,7 @@ if game.PlaceId == 9551640993 then
 		local BlocksPer1 = count33 - count3
 		count3 = count3 + BlocksPer1
 		
-        local Webhooksss = _G.Webhookss
+        local Webhooksss = _G.SettingsTable.Webhookssss
         local OSTime = os.time();
         local Time = os.date('!*t', OSTime);
         timestamp = string.format('%d-%d-%dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec);
@@ -117,32 +114,32 @@ if game.PlaceId == 9551640993 then
                     },
                     ["fields"] = {},
                     ["footer"] = {
-                        ["text"] = "Updates every " .. _G.Intervals .. " Seconds | Mining Simulator 2"
+                        ["text"] = "Updates every " .. _G.SettingsTable.Intervals .. " Seconds | Mining Simulator 2"
                     },
                     ['timestamp'] = timestamp,
                 }
             }
         }
-        if _G.EggCount == true then
+        if _G.SettingsTable.EggCount == true then
             local EggsCountEmbed = {
             	["name"] = "Egg Count Data",
-            	["value"] = abb(count1) .. " Eggs have been hatched! | *+" .. abb(EggsHatchedPer1) .. " in the last " .. _G.Intervals .. " seconds*",
+            	["value"] = abb(count1) .. " Eggs have been hatched! | *+" .. abb(EggsHatchedPer1) .. " in the last " .. _G.SettingsTable.Intervals .. " seconds*",
             	["inline"] = false
         	    }
             	table.insert(msg["embeds"][1]["fields"], EggsCountEmbed)
         end
-	    if _G.RebirthCount == true then
+	    if _G.SettingsTable.RebirthCount == true then
 	        local RebirthsCountEmbed = {
 	            ["name"] = "Rebirth Count Data",
-				["value"] = abb(count22) .. " Rebirths have been counted! | *+" .. abb(RebirthsPer1) .. " in the last " .. _G.Intervals .. " seconds*",
+				["value"] = abb(count22) .. " Rebirths have been counted! | *+" .. abb(RebirthsPer1) .. " in the last " .. _G.SettingsTable.Intervals .. " seconds*",
 				["inline"] = false
 	            }
 	            table.insert(msg["embeds"][1]["fields"], RebirthsCountEmbed)
 	    end
-	    if _G.BlocksCount == true then
+	    if _G.SettingsTable.BlocksCount == true then
 	        local BlocksCountEmbed = {
 	            ["name"] = "Block Count Data",
-				["value"] = abb(count33) .. " Blocks have been mined! | *+" .. abb(BlocksPer1) .. " in the last " .. _G.Intervals .. " seconds*",
+				["value"] = abb(count33) .. " Blocks have been mined! | *+" .. abb(BlocksPer1) .. " in the last " .. _G.SettingsTable.Intervals .. " seconds*",
 				["inline"] = false
 	            }
 	            table.insert(msg["embeds"][1]["fields"], BlocksCountEmbed)
